f4f57e0e3cc6b7d375386d188caf57da7df33bdb
juwon <58173061+juwon0605@users.noreply.github.com>
2022-05-10 01:43:31 +0900

    완주하지 못한 선수

--- src/Main.java
+++ src/Main.java
@@ -6,3 +6,37 @@ public class Main {
 
     }
 }
+
+class Solution {
+    public String solution(String[] participants, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> participantCount = new HashMap<>();
+
+        for(String participant : participants){
+            int count = participantCount.getOrDefault(participant, 0);
+            participantCount.put(participant, ++count);
+        }
+
+        for(String finisher : completion){
+            int count = participantCount.get(finisher);
+            participantCount.put(finisher, --count);
+        }
+
+        for(Map.Entry<String, Integer> entry : participantCount.entrySet()){
+            if(entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+/*
+모범 답안
+    시간복잡도
+        O(N)
+    공간복잡도
+        O(N)
+ */
